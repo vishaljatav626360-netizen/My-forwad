b3b47a19d2262a03fc336a8c77c662e6aa7ec38d
vishaljatav626360-netizen <vishaljatav626360@gmail.com>
2026-02-12 18:17:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from pyrogram import Client, filters
 import os
+from flask import Flask
+from threading import Thread
 
-# Naye Credentials
+# Web Server setup taaki Render bot ko band na kare
+web = Flask('')
+
+@web.route('/')
+def home():
+    return "Bot is Alive!"
+
+def run_web():
+    web.run(host='0.0.0.0', port=8080)
+
+# Credentials
 API_ID = 32920807
 API_HASH = "43c7e7141a55e7ebece4e577fc808a7b"
 SESSION = os.environ.get("SESSION_STRING") 
@@ -14,10 +26,4 @@ app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, session_string=SESSION)
 @app.on_message(filters.chat(SOURCE))
 async def forward(client, message):
     try:
-        # Copying without downloading (0% storage)
         await message.copy(chat_id=TARGET)
-    except Exception as e:
-        print(f"Error: {e}")
-
-print("🚀 Cloud Forwarder is Running...")
-app.run()
