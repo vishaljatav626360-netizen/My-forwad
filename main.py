25097e42c89aba1ee7debd4dc379a0025eb88f64
vishaljatav626360-netizen <vishaljatav626360@gmail.com>
2026-02-12 18:07:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from pyrogram import Client, filters
 import os
 
-# Credentials
-API_ID = 21975825 
-API_HASH = "85223c21c60a9202167d6092d6e38b36"
-# Render par hum SESSION_STRING use karenge taaki login baar-baar na karna pade
+# Naye Credentials
+API_ID = 32920807
+API_HASH = "43c7e7141a55e7ebece4e577fc808a7b"
 SESSION = os.environ.get("SESSION_STRING") 
 
 SOURCE = -1002050954054
@@ -15,6 +14,7 @@ app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, session_string=SESSION)
 @app.on_message(filters.chat(SOURCE))
 async def forward(client, message):
     try:
+        # Copying without downloading (0% storage)
         await message.copy(chat_id=TARGET)
     except Exception as e:
         print(f"Error: {e}")
